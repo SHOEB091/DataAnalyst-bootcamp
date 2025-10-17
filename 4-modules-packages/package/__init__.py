53b1504d721522817e0b8506ecda2cc0dd0e4a66
shoeb <wiqbal091@gmail.com>
2025-10-18 01:57:19 +0530

    Filter function and modules and packages

---
+++ 4-modules-packages/package/__init__.py
@@ -0,0 +1,2 @@
+'''___init__.oy is a special file used in python to define a package and initialize their namespaces'''
+# to initialize a package, we can use __init__.py file
---
+++ 4-modules-packages/package/maths.py
@@ -0,0 +1,21 @@
+def addition(a, b):
+    return a + b
+
+def subtraction(a, b):
+    return a - b
+
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+def power(a, b):
+    return a ** b
+def modulus(a, b):
+    return a % b
+def floor_division(a, b):
+    return a // b
+def square_root(a):
+    return a ** 0.5
+def cube_root(a):
+    return a ** (1/3)
